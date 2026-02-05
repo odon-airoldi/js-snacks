a54f22148a7d4e3636a8c82a878a7c37fe7dacbe
odon-airoldi <odon.airoldi@gmail.com>
2026-02-05 12:47:59 +0100

    exercise snack-4

--- snack-3/assets/js/main.js
+++ snack-3/assets/js/main.js
@@ -1,7 +1,6 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-
 const numeri = [prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'), prompt('Numero'),  prompt('Numero')]
 
 let sommaNumeri = 0;
@@ -13,3 +12,7 @@ for (i = 0; i < numeri.length; i++ ) {
 console.log(sommaNumeri);
 
 
+// correzione
+for (i = 0; i < 10; i++ ) {
+
+}
---
+++ snack-4/assets/js/main.js
@@ -0,0 +1,17 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+
+let numeriDispari = []
+
+for (i = 0; i < 6; i++ ) {
+
+    const numero = Number(prompt('inserisci un numero'));
+    if (numero % 2 == 0) {
+        numeriDispari.push(numero);
+    }
+
+}
+
+console.log(numeriDispari);
