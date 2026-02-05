10b45f41c9dfbe88c25f0ef9b8051202f4c69174
odon-airoldi <odon.airoldi@gmail.com>
2026-02-05 11:02:11 +0100

    exercise correction snack-1

--- snack-1/assets/js/main.js
+++ snack-1/assets/js/main.js
@@ -5,8 +5,10 @@ const secondoNumero = Number(prompt('Inserisci un altro numero'));
 
 if (primoNumero > secondoNumero) {
     numeroMaggiore = primoNumero;
-} else {
+} else if {
     numeroMaggiore = secondoNumero;
+} else {
+    numeroMaggiore = `${primoNumero} è uguale a ${secondoNumero}`;
 }
 
 console.log(numeroMaggiore);
