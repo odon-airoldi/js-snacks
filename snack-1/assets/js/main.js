008cbc4b50c3a450e0102a67c9bd125aabbd7c66
odon-airoldi <odon.airoldi@gmail.com>
2026-02-05 11:04:29 +0100

    follows previous commit

--- snack-1/assets/js/main.js
+++ snack-1/assets/js/main.js
@@ -4,9 +4,9 @@ const primoNumero = Number(prompt('Inserisci un numero'));
 const secondoNumero = Number(prompt('Inserisci un altro numero'));
 
 if (primoNumero > secondoNumero) {
-    numeroMaggiore = primoNumero;
-} else if {
-    numeroMaggiore = secondoNumero;
+    numeroMaggiore = `${primoNumero} è maggiore di ${secondoNumero}`;
+} else if (secondoNumero > primoNumero) {
+    numeroMaggiore = `${secondoNumero} è maggiore di ${primoNumero}`;
 } else {
     numeroMaggiore = `${primoNumero} è uguale a ${secondoNumero}`;
 }
